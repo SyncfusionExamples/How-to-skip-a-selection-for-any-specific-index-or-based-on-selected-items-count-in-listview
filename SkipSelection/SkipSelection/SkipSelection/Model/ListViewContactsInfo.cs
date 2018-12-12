b91578858175c6022b8904293c39c5ec186a3219
Gnana Priya <gnanapriya.namasivayam@syncfusion.com>
2018-12-12 12:43:48 +0530

    Removed unwanted codes from sample.

--- SkipSelection/SkipSelection/SkipSelection/Model/ListViewContactsInfo.cs
+++ SkipSelection/SkipSelection/SkipSelection/Model/ListViewContactsInfo.cs
@@ -16,7 +16,6 @@ namespace Grouping
 
         private string contactName;
         private string contactNo;
-        private ImageSource image;
         private string contactType;
 
         #endregion
@@ -58,16 +57,6 @@ namespace Grouping
             {
                 this.contactType = value;
                 RaisePropertyChanged("ContactType");
-            }
-        }
-
-        public ImageSource ContactImage
-        {
-            get { return this.image; }
-            set
-            {
-                this.image = value;
-                this.RaisePropertyChanged("ContactImage");
             }
         }
 
